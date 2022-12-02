16f4be0e7ed06fe6320133254d1ffd6c2feb6c79
Rani <rutavaghasiya2339gmail.com@Mac.local>
2022-12-02 13:31:31 -0500

    Add new file 12.py

---
+++ 12.py
@@ -0,0 +1,19 @@
+import shutil
+import sys
+
+
+def check_disk_usage(disk, min_absolute, min_percentage):
+    du = shutil.disk_usage(disk)
+
+    percent_free = 100 * du.free / du.total
+    gb_free = du.free / 2 ** 30
+    if percent_free < min_percentage or gb_free < min_absolute:
+        return False
+    else:
+        return True
+
+
+if not check_disk_usage("/", 2, 10):
+    print("Not enough")
+
+print("Ok")
